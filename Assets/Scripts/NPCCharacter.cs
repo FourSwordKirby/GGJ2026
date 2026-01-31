8a99edd8c191b6d54fd48e60651bbe056d65d15d
DaiDiimon <imonh222@gmail.com>
2026-01-31 13:44:07 -0800

    Grid spawn code

--- Assets/Scripts/NPCCharacter.cs
+++ Assets/Scripts/NPCCharacter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public enum CliqueKind
 {
@@ -21,6 +22,17 @@ public class NPCCharacter : MonoBehaviour
     private Rigidbody rb;
     private float spawnTime;
 
+    // Cheerleader
+
+    private float cheerHopTime;
+
+    // Nerd
+
+    private float nerdMarchDuration;
+    private float nerdTargetRotation;
+
+    // Methods
+
     public void SetClique(CliqueKind cliqueKind)
     {
         this.cliqueKind = cliqueKind;
@@ -31,6 +43,62 @@ public class NPCCharacter : MonoBehaviour
         // npcZone = GetComponentInParent<NPCZone>();
         rb = GetComponent<Rigidbody>();
         spawnTime = Time.time;
+
+        InitTransform();
+    }
+
+    void InitTransform()
+    {
+        switch (cliqueKind)
+        {
+            case CliqueKind.JOCK:
+                {
+                    // Randomly rotate pivot by rotating the NPC
+
+                    transform.Rotate(Vector3.up, Random.value * 360.0f);
+                    rb.linearVelocity = transform.forward * npcSettings.jockRunVelocity;
+                }
+                break;
+
+            case CliqueKind.CHEERLEADER:
+                {
+                    // Start in right hop position
+
+                    transform.position += Vector3.right * (npcSettings.cheerHopDist / 2.0f);
+
+                    cheerHopTime = Time.time;
+                }
+                break;
+
+            case CliqueKind.BUSINESS:
+                {
+                    // Randomize into one of four cardinals
+
+                    int signX = (Random.value < 0.5f) ? 1 : -1;
+                    int signZ = (Random.value < 0.5f) ? 1 : -1;
+                    Vector3 normalFace = new Vector3(signX, 0, signZ);
+                    transform.rotation = Quaternion.LookRotation(normalFace);
+
+                    rb.linearVelocity = -transform.forward * npcSettings.businessWalkSpeed;
+                }
+                break;
+
+            case CliqueKind.NERD:
+                {
+                    // Randomize orientation, init tracking variables
+
+                    transform.Rotate(Vector3.up, Random.value * 360.0f);
+
+
+                }
+                break;
+
+            case CliqueKind.THEATER:
+                {
+                    rb.linearVelocity = TheaterDirSign() * transform.right * npcSettings.theaterMoveSpeed;
+                }
+                break;
+        }
     }
 
     void Update()
@@ -60,7 +128,12 @@ public class NPCCharacter : MonoBehaviour
                     float accel = npcSettings.JockCentripetalAccel();
 
                     Vector3 velocityNext = velocityCur + Time.fixedDeltaTime * normalCur * accel;
+                    velocityNext.y = 0;
                     rb.linearVelocity = velocityNext;
+
+                    // Look towards velocity
+
+                    transform.rotation = Quaternion.LookRotation(velocityCur);
                 }
                 break;
 
@@ -71,6 +144,7 @@ public class NPCCharacter : MonoBehaviour
 
                     // Hop, hop, left, hop, hop, right
 
+                    float dTFromTime = cheerHopTime - Time.time;
                 }
                 break;
 
@@ -81,6 +155,11 @@ public class NPCCharacter : MonoBehaviour
                         return;
 
                     // Move back til edge
+
+                    if (npcZone)
+                    {
+
+                    }
                 }
                 break;
 
@@ -89,7 +168,7 @@ public class NPCCharacter : MonoBehaviour
                     if (fFixedUpdate)
                         return;
 
-                    // Periodically 
+                    // Periodically pause and rotate
                 }
                 break;
 
@@ -98,10 +177,17 @@ public class NPCCharacter : MonoBehaviour
                     if (fFixedUpdate)
                         return;
 
-                    // Change direction on timer
-
+                    rb.linearVelocity = TheaterDirSign() * transform.right * npcSettings.theaterMoveSpeed;
                 }
                 break;
         }
+    }
+
+    // Theater
+
+    float TheaterDirSign()
+    {
+        float dTSpawn = Time.time - spawnTime;
+        return (Mathf.FloorToInt(dTSpawn / npcSettings.theaterSwitchTime) % 2 == 0) ? -1 : 1;
     }
 }
--- Assets/Scripts/NPCZone.cs
+++ Assets/Scripts/NPCZone.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class NPCZone : MonoBehaviour
 {
@@ -10,9 +11,13 @@ public class NPCZone : MonoBehaviour
     [SerializeField] private NPCSettings npcSettings;
     [SerializeField] private ReferenceLibrary referenceLibrary;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rect rectBounds;
+    private Transform spawnGroup;
+
     void Start()
     {
+        spawnGroup = (new GameObject("NPCZoneSpawnGroup")).transform;
+        RefreshRectBounds();
         SpawnCrowd();
     }
 
@@ -22,41 +27,86 @@ public class NPCZone : MonoBehaviour
         {
             case CliqueKind.JOCK:
                 {
-                    SpawnNPCGrid(density: 20, noise: 0);
+                    SpawnNPCGrid();
                 }
                 break;
 
             case CliqueKind.CHEERLEADER:
                 {
-                    SpawnNPCGrid(density: 20, noise: 0);
+                    SpawnNPCGrid();
 
                 }
                 break;
 
             case CliqueKind.BUSINESS:
                 {
-                    SpawnNPCGrid(density: 20, noise: 0);
-
+                    SpawnNPCGrid();
                 }
                 break;
 
             case CliqueKind.NERD:
                 {
-                    SpawnNPCGrid(density: 20, noise: 0);
+                    SpawnNPCGrid();
                 }
                 break;
 
             case CliqueKind.THEATER:
                 {
-                    SpawnNPCGrid(density: 20, noise: 0);
+                    float marginHori = npcSettings.theaterMoveSpeed * npcSettings.theaterSwitchTime * 1.05f;
+                    SpawnNPCGrid(marginHori: marginHori);
                 }
                 break;
         }
     }
 
-    void SpawnNPCGrid(float density, float noise)
+    void SpawnNPCGrid(
+        float unitSize = 1,
+        float offsetLimit = 1,
+        float populationRatio = 1.0f,
+        float marginHori = 1.0f,
+        float marginVert = 1.0f)
     {
-        // density = 
+        // Get spawn points
+
+        float width = rectBounds.width - (marginHori * 2);
+        float height = rectBounds.height - (marginVert * 2);
+        float xMin = transform.position.x - (width / 2) + (unitSize / 2);
+        float zMin = transform.position.z - (height / 2) + (unitSize / 2);
+        Vector3 spawnMin = new Vector3(xMin, transform.position.y, zMin);
+
+        int rowCount = Mathf.FloorToInt(width / unitSize);
+        int colCount = Mathf.FloorToInt(height / unitSize);
+
+        int totalCount = rowCount * colCount;
+        int pullCount = Mathf.RoundToInt(populationRatio * totalCount);
+
+        List<int> spawnIndices = PullFromShuffledDeck(pullCount, 0, totalCount);
+
+        // Spawn at points
+
+        foreach (int spawnIndex in spawnIndices)
+        {
+            int rowIndex = spawnIndex / rowCount;
+            int colIndex = spawnIndex % rowCount;
+
+            Vector2 dPosSpawn = Vector2.right * unitSize * colIndex + Vector2.up * unitSize * rowIndex;
+            dPosSpawn += Random.insideUnitCircle * offsetLimit;
+
+            float clampedX = Mathf.Clamp(dPosSpawn.x, -width / 2, width / 2);
+            float clampedY = Mathf.Clamp(dPosSpawn.y, -height / 2, height / 2);
+
+            dPosSpawn = new Vector2(clampedX, clampedY);
+
+            Vector3 spawnPos = spawnMin + new Vector3(dPosSpawn.x, 0, dPosSpawn.y);
+
+            Instantiate(referenceLibrary.npcPrefab, spawnPos, Quaternion.identity, spawnGroup);
+        }
+    }
+
+    void RefreshRectBounds()
+    {
+        Vector3 vecScale = transform.localScale;
+        this.rectBounds = new Rect(transform.position, new Vector2(vecScale.x, vecScale.z));
     }
 
     // Gizmos
@@ -65,5 +115,26 @@ public class NPCZone : MonoBehaviour
     {
         Gizmos.color = new Color(0, 0, 1, 0.5f);
         Gizmos.DrawCube(transform.position, transform.localScale);
+    }
+
+    // Util
+
+    List<int> PullFromShuffledDeck(int pullCount, int rangeMin, int rangeMax)
+    {
+        List<int> deck = new List<int>(rangeMax - rangeMin);
+        for (int i = rangeMin; i < rangeMax; i++)
+        {
+            deck.Add(i);
+        }
+
+        List<int> result = new List<int>();
+        for (int i = 0; i < pullCount; i++)
+        {
+            int randomIndex = Random.Range(0, deck.Count);
+            result.Add(deck[randomIndex]);
+
+            deck.RemoveAt(randomIndex);
+        }
+        return result;
     }
 }
