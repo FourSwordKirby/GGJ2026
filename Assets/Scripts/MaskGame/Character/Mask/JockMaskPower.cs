3b0557fa7de8e5ba63fde49d3ac49c4f20b21b30
Swofty <swiftstick@gmail.com>
2026-02-01 00:32:35 -0800

    Fix jock getting stuck on wall

--- Assets/Scripts/MaskGame/Character/Mask/JockMaskPower.cs
+++ Assets/Scripts/MaskGame/Character/Mask/JockMaskPower.cs
@@ -6,10 +6,16 @@ namespace MaskGame.Character.Mask
     public class JockMaskPower : MaskPower
     {
         public override MaskState MaskState => MaskState.JOCK;
+        public CircleMovementModifier CircleMod;
 
         public JockMaskPower()
         {
-            Movement = new CircleMovementModifier();
+            Movement = CircleMod = new CircleMovementModifier();
+        }
+
+        public override void OnEnterPower(PlayerCharacter character)
+        {
+            CircleMod.LastFacingDir = character.GetFacingDirection();
         }
     }
 }
--- Assets/Scripts/MaskGame/Character/Modifier/CircleMovementModifier.cs
+++ Assets/Scripts/MaskGame/Character/Modifier/CircleMovementModifier.cs
@@ -8,31 +8,58 @@ namespace MaskGame.Character.Modifier
         public float CircleRadius = 1;
         public float MaxSpeed = 5;
         public float Acceleration = 200;
+        public float DegreesPerSecond = 180;
+
+        public Vector3 LastFacingDir;
+        public float DegreesToTravel;
 
         public override void ApplyInputToCharacter(MaskGameCharacter character, CharacterInputs inputs, float deltaTime)
         {
-            Vector3 pivotDirection = -character.ExtendedRigidbody.transform.right;
+            float rotSpeed = DegreesPerSecond;
             bool hasInput = inputs.MovementIntention.magnitude > .2;
             if (hasInput)
             {
-                pivotDirection *= -1;
+                rotSpeed *= -1;
             }
-            Vector3 charToCirclePivot = pivotDirection * CircleRadius;
-            Vector3 circlePivot = character.ExtendedRigidbody.Position + charToCirclePivot;
-            float travelDistance = MaxSpeed * deltaTime;
-            float degreesTraveled = travelDistance / (2.0f * (float)Math.PI * CircleRadius) * 360.0f;
-            if (!hasInput)
-            {
-                degreesTraveled *= -1;
-            }
-            Quaternion rot = Quaternion.Euler(0.0f, degreesTraveled, 0.0f);
-            Vector3 pivotToNewLoc = rot * (-charToCirclePivot);
-            Vector3 newLoc = circlePivot + pivotToNewLoc;
-            Vector3 charToNewLoc = (newLoc - character.ExtendedRigidbody.Position).normalized;
-            
-            Vector3 impulse = ComputeMovementImpulse(character, charToNewLoc, deltaTime);
+            Quaternion rot = Quaternion.Euler(0.0f, rotSpeed * deltaTime, 0.0f);
+            Vector3 newFacingDirection = rot * LastFacingDir;
+            character.SetFacingDirection(newFacingDirection);
+            Vector3 impulse = ComputeMovementImpulse(character, newFacingDirection, deltaTime);
             character.ExtendedRigidbody.ApplyImpulse(impulse, true);
-            character.SetFacingDirection(charToNewLoc);
+            LastFacingDir = newFacingDirection;
+
+            //bool doubleRotation = false;
+            //// Fix for stuck at wall
+            //float rotationChangeSinceLastFrame = Vector3.Angle(LastFacingDir, character.GetFacingDirection());
+            //if (rotationChangeSinceLastFrame < Math.Abs(DegreesToTravel) * .2)
+            //{
+            //    character.SetFacingDirection(LastFacingDir);
+            //    doubleRotation = true;
+            //}
+            //LastFacingDir = character.GetFacingDirection();
+
+            //Vector3 pivotDirection = -Vector3.Cross(Vector3.up, LastFacingDir);
+            //Vector3 charToCirclePivot = pivotDirection * CircleRadius;
+            //Vector3 circlePivot = character.ExtendedRigidbody.Position + charToCirclePivot;
+            //float travelDistance = MaxSpeed * deltaTime;
+            //float degreesTraveled = travelDistance / (2.0f * (float)Math.PI * CircleRadius) * 360.0f;
+            //if (!hasInput)
+            //{
+            //    degreesTraveled *= -1;
+            //}
+            //if (doubleRotation)
+            //{
+            //    degreesTraveled *= 8;
+            //}
+            //DegreesToTravel = degreesTraveled;
+            //Quaternion rot = Quaternion.Euler(0.0f, degreesTraveled, 0.0f);
+            //Vector3 pivotToNewLoc = rot * (-charToCirclePivot);
+            //Vector3 newLoc = circlePivot + pivotToNewLoc;
+            //Vector3 charToNewLoc = (newLoc - character.ExtendedRigidbody.Position).normalized;
+
+            //Vector3 impulse = ComputeMovementImpulse(character, charToNewLoc, deltaTime);
+            //character.ExtendedRigidbody.ApplyImpulse(impulse, true);
+            //character.SetFacingDirection(charToNewLoc);
         }
 
         protected virtual Vector3 ComputeMovementImpulse(MaskGameCharacter character, Vector3 intentedMove, float deltaTime)
