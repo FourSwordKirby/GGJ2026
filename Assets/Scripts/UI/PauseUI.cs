ad0546d36ce97aa21f9c2d654ffa2fc32a615727
DaiDiimon <imonh222@gmail.com>
2026-02-01 14:29:18 -0800

    pause screen

---
+++ Assets/Scripts/UI/PauseUI.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class PauseUI : MonoBehaviour
+{
+	Animator animator;
+
+	bool paused = false;
+
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	bool wasPressed(string actionName)
+	{
+		// Needed as IsPressed is true beyond the first frame
+
+		InputAction action = InputSystem.actions.FindAction(actionName);
+		return action.triggered && action.ReadValue<float>() > 0;
+	}
+
+	void Update()
+	{
+		if (paused)
+		{
+			if (wasPressed("Pause") ||
+				wasPressed("Toggle") ||
+				wasPressed("Cancel"))
+			{
+				SetPause(false);
+			}
+		}
+		else
+		{
+			if (wasPressed("Pause"))
+			{
+				// Only allow during gameplay
+
+				GameManager.GamePhase phase = GameManager.instance.currentPhase;
+
+				if (phase != GameManager.GamePhase.PeriodInProgress)
+					return;
+
+				SetPause(true);
+			}
+		}
+	}
+
+	void SetPause(bool pausedNext)
+	{
+		if (paused == pausedNext)
+			return;
+
+		paused = pausedNext;
+		animator.SetBool("paused", paused);
+
+		if (paused)
+		{
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Time.timeScale = 1;
+		}
+	}
+}
